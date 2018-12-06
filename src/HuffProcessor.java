34011556b41aec0e5c38af7d70fa15c38ba5f3ef
YuvalMedina <yuval.medina@duke.edu>
2018-12-06 02:11:57 -0500

    going to gradescope test compress again

--- src/HuffProcessor.java
+++ src/HuffProcessor.java
@@ -135,7 +135,7 @@ public class HuffProcessor {
 		while (pq.size() > 1) {
 			HuffNode left = pq.remove();
 			HuffNode right = pq.remove();
-			HuffNode t = new HuffNode(0,left.myWeight + right.myWeight);
+			HuffNode t = new HuffNode(0,left.myWeight + right.myWeight,left,right);
 			pq.add(t);
 		}
 		
